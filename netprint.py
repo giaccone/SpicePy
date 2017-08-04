1aba519538583f11821d11dfea77c1d853680948
Luca Giaccone <luca.giaccone@polito.it>
2017-08-04 15:24:13 +0200

    print_util.py -> netprint.py (& small changes)

--- netprint.py
+++ netprint.py
@@ -1,82 +1,82 @@
-def print_branch_voltage(network):
+def print_branch_voltage(net):
     """
     'print_branch_voltage' print the branch voltages on terminal
 
     """
 
     # if necessary reorder
-    if not hasattr(network, 'isort'):
-        network.reorder()
+    if net.isort is None:
+        net.reorder()
 
     print('=====================')
     print('   branch voltages   ')
     print('=====================')
 
-    for k, index in enumerate(network.isort):
+    for k, index in enumerate(net.isort):
         if k == 0: # resistors
             for h in index:
-                print('v({}) = {:10.4f} V'.format(network.names[h], network.vb[h]))
+                print('v({}) = {:10.4f} V'.format(net.names[h], net.vb[h]))
         elif k == 1: # voltage sources
             for h in index:
-                print('v({}) = {:10.4f} V'.format(network.names[h], network.vb[h]))
+                print('v({}) = {:10.4f} V'.format(net.names[h], net.vb[h]))
         elif k == 2: # current sources
             for h in index:
-                print('v({}) = {:10.4f} V'.format(network.names[h], network.vb[h]))
+                print('v({}) = {:10.4f} V'.format(net.names[h], net.vb[h]))
                 print('=====================')
 
 
-def print_branch_current(network):
+def print_branch_current(net):
     """
     'print_branch_current' print the branch voltages on terminal
 
     """
 
     # if necessary reorder
-    if not hasattr(network, 'isort'):
-        network.reorder()
+    if net.isort is None:
+        net.reorder()
 
     print('=====================')
     print('   branch currents   ')
     print('=====================')
 
-    for k, index in enumerate(network.isort):
+    for k, index in enumerate(net.isort):
         if k == 0: # resistors
             for h in index:
-                print('i({}) = {:10.4f} A'.format(network.names[h], network.ib[h]))
+                print('i({}) = {:10.4f} A'.format(net.names[h], net.ib[h]))
         elif k == 1: # voltage sources
             for h in index:
-                print('i({}) = {:10.4f} A'.format(network.names[h], network.ib[h]))
+                print('i({}) = {:10.4f} A'.format(net.names[h], net.ib[h]))
         elif k == 2: # current sources
             for h in index:
-                print('i({}) = {:10.4f} A'.format(network.names[h], network.ib[h]))
+                print('i({}) = {:10.4f} A'.format(net.names[h], net.ib[h]))
                 print('=====================')
 
 
-def print_branch_quantity(network):
+def print_branch_quantity(net):
     """
     'print_branch_quantity' print the branch voltages and currents on terminal
 
     """
 
     # if necessary reorder
-    if not hasattr(network, 'isort'):
-        network.reorder()
+    if net.isort is None:
+        net.reorder()
 
     print('==============================================')
     print('               branch quantities              ')
     print('==============================================')
 
-    for k, index in enumerate(network.isort):
+    for k, index in enumerate(net.isort):
         if k == 0: # resistors
             for h in index:
-                print('v({}) = {:10.4f} V'.format(network.names[h], network.vb[h]), end='    ')
-                print('i({}) = {:10.4f} A'.format(network.names[h], network.ib[h]))
+                print('v({}) = {:10.4f} V'.format(net.names[h], net.vb[h]), end='    ')
+                print('i({}) = {:10.4f} A'.format(net.names[h], net.ib[h]))
         elif k == 1: # voltage sources
             for h in index:
-                print('v({}) = {:10.4f} V'.format(network.names[h], network.vb[h]), end='    ')
-                print('i({}) = {:10.4f} A'.format(network.names[h], network.ib[h]))
+                print('v({}) = {:10.4f} V'.format(net.names[h], net.vb[h]), end='    ')
+                print('i({}) = {:10.4f} A'.format(net.names[h], net.ib[h]))
         elif k == 2: # current sources
             for h in index:
-                print('v({}) = {:10.4f} V'.format(network.names[h], network.vb[h]), end='    ')
-                print('i({}) = {:10.4f} A'.format(network.names[h], network.ib[h]))
+                print('v({}) = {:10.4f} V'.format(net.names[h], net.vb[h]), end='    ')
+                print('i({}) = {:10.4f} A'.format(net.names[h], net.ib[h]))
                 print('==============================================')
