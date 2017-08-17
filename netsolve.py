5a5db293ac50f5aedf84761675ff49b91b895eab
Luca Giaccone <luca.giaccone@polito.it>
2017-08-17 15:56:26 +0200

    in netsolve.py: +ac_solve +net_solve

--- netsolve.py
+++ netsolve.py
@@ -1,4 +1,5 @@
 from scipy.sparse.linalg import spsolve
+import numpy as np
 
 def dc_solve(net):
     """
@@ -13,3 +14,31 @@ def dc_solve(net):
 
     # linear system definition
     net.x = spsolve(net.G, net.rhs)
+
+def ac_solve(net):
+    """
+
+    :param net:
+    :return:
+    """
+
+    net.conductance_matrix()
+    net.dynamic_matrix()
+    net.rhs_matrix()
+
+    # frequency
+    f = float(net.analysis[-1])
+
+    # linear system definition
+    net.x = spsolve(net.G + 1j * 2 * np.pi * f* net.C, net.rhs)
+
+def net_solve(net):
+    """
+    
+    :param net:
+    :return:
+    """
+    if net.analysis[0] == '.op':
+        dc_solve(net)
+    elif net.analysis[0] == '.ac':
+        ac_solve(net)
